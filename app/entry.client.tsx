abbdfa623ab0b1b1e88089f018408177c1de4e81
tHyt-lab <washers.fezzes-0q@icloud.com>
2023-03-06 15:00:12 +0000

    first commit

---
+++ app/entry.client.tsx
@@ -0,0 +1,25 @@
+import { NextUIProvider } from "@nextui-org/react";
+import { RemixBrowser } from "@remix-run/react";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <NextUIProvider>
+          <RemixBrowser />
+        </NextUIProvider>
+      </StrictMode>
+    );
+  });
+}
+
+if (typeof requestIdleCallback === "function") {
+  requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  setTimeout(hydrate, 1);
+}
---
+++ app/root.tsx
@@ -0,0 +1,40 @@
+import { CssBaseline } from "@nextui-org/react";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
+
+import type { LinksFunction } from "@remix-run/server-runtime";
+import globalLargeStylesUrl from "./styles/global-large.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalStylesUrl from "./styles/global.css";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: globalStylesUrl },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)",
+    },
+  ];
+};
+
+export default function App() {
+  return (
+    <html lang="ja">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
+        {CssBaseline.flush()}
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
